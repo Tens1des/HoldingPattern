91a538a180b5a703271d3fe28c147ae3be63426d
Tens1de <romankotov3213@gmail.com>
2026-02-22 20:03:21 +0300

    Initial Commit

---
+++ HoldingPattern/HoldingPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoldingPatternApp.swift
+//  HoldingPattern
+//
+//  Created by dad on 22.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct HoldingPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
